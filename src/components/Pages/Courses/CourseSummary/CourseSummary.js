014ce8caa57a135f44a591f97da7e9315d093c53
grzafnan <grzafnan@gmail.com>
2022-12-18 03:10:58 +0600

    details page update

--- src/components/Pages/Courses/CourseSummary/CourseSummary.js
+++ src/components/Pages/Courses/CourseSummary/CourseSummary.js
@@ -9,59 +9,98 @@ const CourseSummary = () => {
 
   const { details, image_url, title, _id, student, price, level, feature } = courseData;
 
-
+  console.log(courseData);
 
   return (
-    <div className=' dark:bg-[#2e2e2e] py-10 dark:text-gray-100'>
-      <div ref={ref} className=" w-3/4 lg:w-3/5 mx-auto p-4 shadow-md dark:bg-gray-900 dark:text-gray-100 rounded-lg">
-        <div className="flex justify-between pb-4 border-bottom">
-          <div className="flex items-center">
-            <h2 className='font-semibold text-lg md:text-2xl '>{title}</h2>
+    <>
+      <section className='dark:bg-[#2e2e2e] dark:text-gray-100'>
+        <div ref={ref} class="relative mx-auto max-w-screen-xl px-4 py-10">
+          <div className='flex justify-between items-start md:items-center'>
+            <h1 class="text-2xl font-bold lg:text-3xl">
+              {title}
+            </h1>
+            <Pdf targetRef={ref} filename={title}>
+              {({ toPdf }) => <button onClick={toPdf}>
+                <FaDownload title='Download Course Details' className='text-green-600 text-2xl md:text-3xl' /></button>
+              }
+            </Pdf>
           </div>
-          <Pdf targetRef={ref} filename="code-example.pdf">
-            {({ toPdf }) => <button onClick={toPdf}>
-              <FaDownload className='text-green-600 text-2xl md:text-3xl' /></button>}
-          </Pdf>
-
-        </div>
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <img src={image_url} alt="" className="block  w-full rounded-md h-72 dark:bg-gray-500" />
-          </div>
-          <div className="space-y-2">
-            <div className='flex justify-between items-center'>
-              <p className='md:text-lg font-semibold'>Level: {level}</p>
-              <p className='md:text-lg font-semibold'>Student: {student}</p>
-              <p className='md:text-lg font-semibold'>Fee: ${price}</p>
-
+          <div class="grid gap-8 lg:grid-cols-4 lg:items-start">
+            <div class="lg:col-span-3">
+              <div class="relative mt-4">
+                <img
+                  alt="Tee"
+                  src={image_url}
+                  class="h-72 w-full rounded-xl object-cover lg:h-[540px]"
+                />
+              </div>
             </div>
 
-
-            <p className="leading-snug text-justify dark:text-gray-400">{details}</p>
-
-            <div className='pt-8'>
-              <h2 className='text-xl font-semibold  underline'>You Will Gain Those Skills From This Course!</h2>
-              <ul className='text-start list-disc p-6'>
-                {
-                  feature?.map((item, idx) => <li
-                    key={idx}
-                    className=''
+            <div class="lg:sticky lg:top-20 lg:pt-8">
+              <div className='md:w-2/4 lg:w-full md:mx-auto lg:mx-0 flex lg:flex-col justify-between items-center lg:justify-start lg:items-start gap-6'>
+                <div className='w-full flex items-center lg:justify-between gap-4'>
+                  <p className='text-lg font-semibold'>
+                    Level
+                  </p>
+                  <span
+                    class="whitespace-nowrap rounded-full bg-blue-100 px-3 py-0.5 text-md font-semibold text-blue-700"
                   >
-
-                    {item}
-                  </li>)
-                }
-              </ul>
-
-              <div className='flex justify-center'>
+                    {level}
+                  </span>
+                </div>
+                <div className='w-full flex items-center lg:justify-between gap-4'>
+                  <p className='text-lg font-semibold'>
+                    Student
+                  </p>
+                  <span
+                    class="whitespace-nowrap rounded-full bg-blue-100 px-3 py-0.5 text-md font-semibold text-blue-700"
+                  >
+                    {student}
+                  </span>
+                </div>
+              </div>
+              <div className='mt-6 lg:mt-96'>
+                <p class="text-xl font-bold mb-5">Price: ${price}</p>
                 <Link to={`../checkout/${_id}`} >
-                  <button className='bg-blue-500 text-white hover:bg-blue-600 font-semibold px-6 py-2 rounded '>Get Premium Access</button></Link>
+                  <button
+                    type="submit"
+                    class="w-full rounded bg-blue-600 hover:bg-blue-700 px-6 py-3 text-sm font-bold uppercase tracking-wide text-white"
+                  >
+                    Get Premium Access
+                  </button>
+                </Link>
+              </div>
+            </div>
+
+            <div class="lg:col-span-3">
+              <div
+                class="prose max-w-none [&>iframe]:mt-6 [&>iframe]:aspect-video [&>iframe]:w-full [&>iframe]:rounded-xl"
+              >
+                <p>
+                  {details}
+                </p>
+                <br />
+                <h2 className='text-xl font-semibold text-blue-600 mb-1'>
+                  You Will Gain Those Skills From This Course!
+                </h2>
+                <ul className='text-start list-disc px-6'>
+                  {
+                    feature?.map((item, idx) => <li
+                      key={idx}
+                      className=''
+                    >
+
+                      {item}
+                    </li>)
+                  }
+                </ul>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+
+    </>
   );
 };
 
